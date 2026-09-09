6f7833384e6a749b779af13bf8ad264514f77e36
jiaxiaohua <>
2026-09-09 17:41:03 +0900

    -

--- deploys/modelscope/download_model.sh
+++ deploys/modelscope/download_model.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 
 #https://www.modelscope.cn/
-export MODEL_ID="$1-Qwen/Qwen3.8-27B"
+export MODEL_ID="${1:-Qwen/Qwen3.8-27B}"
 
 export MODEL_ROOT_DIR="/data/models"
 
 # pip install -U modelscope-hub
 
-mkdir -p "MODEL_ROOT_DIR/$MODEL_ID"
+mkdir -p "$MODEL_ROOT_DIR/$MODEL_ID"
 
-ms-hub download "$MODEL_ID" --local-dir "MODEL_ROOT_DIR/$MODEL_ID"
+ms-hub download "$MODEL_ID" --local-dir "$MODEL_ROOT_DIR/$MODEL_ID"
--- deploys/vllm/docker_run_vllm_qwen.sh
+++ deploys/vllm/docker_run_vllm_qwen.sh
@@ -3,11 +3,11 @@
 set -x
 set -euo pipefail
 
-export MODEL_ID="$1:-Qwen/Qwen3.8-27B"
+export MODEL_ID="${1:-Qwen/Qwen3.8-27B}"
 export MODEL_ROOT_DIR="/data/models"
-export RUN_NAME="$2:-vllm_qwen38_27b"
+export RUN_NAME="${2:-vllm-qwen38-27b}"
 
-docker run -name $RUN_NAME --gpus all \
+docker run --name $RUN_NAME --gpus all \
   -p 18081:8000 \
   -v ${MODEL_ROOT_DIR}/${MODEL_ID}:/model \
   vllm/vllm-openai:latest \
