b595f533811c71fef17fdbf7e0a807d689f4e826
jiaxiaohua <>
2026-09-10 13:57:07 +0900

    -

--- deploys/vllm/docker_run_vllm_qwen.sh
+++ deploys/vllm/docker_run_vllm_qwen.sh
@@ -12,8 +12,11 @@ docker run --name $RUN_NAME --gpus all \
   -v ${MODEL_ROOT_DIR}/${MODEL_ID}:/model \
   vllm/vllm-openai:latest \
   --model /model \
-  --tensor-parallel-size 4 \
-  --max-model-len 262144 \
+  --tensor-parallel-size 1 \
+  --max-model-len 65536 \
+  --gpu-memory-utilization 0.98 \
+  --max-num-seqs 8 \
+  --enforce-eager \
   --served-model-name "`basename $MODEL_ID`" \
   --enable-auto-tool-choice \
   --tool-call-parser qwen3_coder \
